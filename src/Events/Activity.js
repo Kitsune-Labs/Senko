0ea989fbe7a14b955baa5fdc0d61ac6f028a653c
Senko <SewayakiSenko@protonmail.com>
2022-07-15 15:23:15 -0700

    Activity & Market Changes

--- src/Events/Activity.js
+++ src/Events/Activity.js
@@ -6,20 +6,6 @@ module.exports = {
      * @param {Client} SenkoClient
      */
 	execute: async (SenkoClient) => {
-		// eslint-disable-next-line no-unused-vars
-		const Status = [
-			{ "activities": [{ "name": "do you think Nakano wants to engulf in my tail again?" }] },
-			{ "activities": [{ "name": "/fluff" }] },
-			{ "activities": [{ "name": "Uya!" }] },
-			{ "activities": [{ "name": "Umu~ Umu~" }] },
-			{ "activities": [{ "name": "nom nom" }] },
-			{ "activities": [{ "name": "poof!" }] },
-			{ "activities": [{ "name": "/pat" }] },
-			{ "activities": [{ "name": "what is Nakano thinking?" }] },
-			{ "activities": [{ "name": "how much is fried tofu?" }] },
-			{ "activities": [{ "name": "ヾ(•ω•`)o" }] }
-		];
-
 		async function switchActivity() {
 			const { data } = await Supabase.from("config").select("*").eq("id", "all");
 
